4a603980eebee5543b0073f69edeeb4a0617d88d
Brian Graham <support@netcoding.net>
2015-04-02 20:17:27 -0400

    Started development

---
+++ src/net/netcoding/niftybans/NiftyBans.java
@@ -0,0 +1,17 @@
+package net.netcoding.niftybans;
+
+import net.netcoding.niftybukkit.minecraft.BukkitPlugin;
+
+public class NiftyBans extends BukkitPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
